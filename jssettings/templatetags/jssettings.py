996fe7c3805a2cd34ffb85975ffa1af8385abb4e
sthzg <sthzg@gmx.net>
2014-09-21 10:05:22 +0200

    Added jss_ready_actions template tage. Renamed jssettings template tag to jss_settings.

--- jssettings/templatetags/jssettings.py
+++ jssettings/templatetags/jssettings.py
@@ -7,7 +7,28 @@ register = template.Library()
 
 
 @register.simple_tag
-def jssettings(request):
+def jss_settings(request):
     return json.dumps(request.js_settings)
 
-register.simple_tag(jssettings)
+register.simple_tag(jss_settings)
+
+
+@register.simple_tag
+def jss_ready_actions(request):
+    if not request.js_settings.get('ready_actions'):
+        return ''
+
+    actions = request.js_settings.get('ready_actions')
+
+    return READY_ACTIONS.format('\r\n'.join(actions))
+
+READY_ACTIONS = '''
+<script type="text/javascript">
+jQuery(document).ready(function()
+{{
+    {0}
+}});
+</script>
+'''
+
+register.simple_tag(jss_ready_actions)
